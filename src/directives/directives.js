2fb76d0200238304c7cad97252f24cfc2138e344
sebgl <contact.sebgl@gmail.com>
2016-07-24 00:25:39 +0200

    build for gh-pages

--- src/directives/directives.js
+++
@@ -1,5 +0,0 @@
-import Vue from 'vue'
-
-Vue.directive('modal-img', function (url) {
-  return (timeMS / 1000 / 60).toFixed(2).toString().replace('.', ':')
-})
--- src/filters/filters.js
+++
@@ -1,5 +0,0 @@
-import Vue from 'vue'
-
-Vue.filter('formatMsTime', function (timeMS) {
-  return (timeMS / 1000 / 60).toFixed(2).toString().replace('.', ':')
-})
